10ce28cd46b22e4bd07b3fa0ee93df38477ca5bc
DCarhuamaca <140361129+Keterod@users.noreply.github.com>
2025-10-30 17:45:18 -0500

    Add files via upload

---
+++ BD_SCRIPTS/STORE PROCEDURES/ConsultarLibroPorID.sql
@@ -0,0 +1,32 @@
+-- ===========================================================
+--  PROCEDIMIENTO ALMACENADO: ConsultarLibroPorID
+-- ===========================================================
+CREATE PROCEDURE ConsultarLibroPorID
+    @id_libro INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+        -- Consultar los datos del libro
+        SELECT 
+            id_libro,
+            titulo,
+            autor,
+            genero,
+            año_publicacion,
+            estado_L,
+            descripcion,
+            imagen_url
+        FROM Libros
+        WHERE id_libro = @id_libro;
+
+        PRINT 'Consulta de libro realizada correctamente.';
+    END TRY
+
+    BEGIN CATCH
+        PRINT 'Error al consultar el libro: ' + ERROR_MESSAGE();
+    END CATCH
+END;
+GO
+
