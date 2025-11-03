49a9153763189f3c6533b734d1f0e28591ac0e74
DCarhuamaca <140361129+Keterod@users.noreply.github.com>
2025-11-02 20:47:40 -0500

    Update AgregarLibro.sql

--- BD_SCRIPTS/STORE PROCEDURES/AgregarLibro.sql
+++ BD_SCRIPTS/STORE PROCEDURES/AgregarLibro.sql
@@ -1,38 +1,18 @@
 -- ===========================================================
 --  PROCEDIMIENTO ALMACENADO: AgregarLibro
 -- ===========================================================
-CREATE PROCEDURE AgregarLibro
+CREATE OR ALTER PROCEDURE AgregarLibro
     @titulo VARCHAR(150),
-    @autor VARCHAR(100),
-    @genero VARCHAR(50) = NULL,  -- Género es opcional
-    @año_publicacion SMALLINT = NULL,  -- Año de publicación es opcional
-    @estado_L VARCHAR(20) = 'Disponible',  -- Estado por defecto
-    @descripcion VARCHAR(500) = NULL,  -- Descripción es opcional
-    @imagen_url VARCHAR(255) = NULL  -- Ruta de la imagen
+    @autor VARCHAR(150),
+    @genero VARCHAR(50),
+    @anio INT,
+    @estado VARCHAR(50),
+    @descripcion VARCHAR(MAX),
+    @imagen VARBINARY(MAX)  -- <-- aquí es lo importante
 AS
 BEGIN
-    -- Comenzar la transacción para asegurar la integridad de los datos
-    BEGIN TRY
-        BEGIN TRANSACTION
-        
-        -- Insertar el nuevo libro en la tabla Libros
-        INSERT INTO Libros (titulo, autor, genero, año_publicacion, estado_L, descripcion, imagen_url)
-        VALUES (@titulo, @autor, @genero, @año_publicacion, @estado_L, @descripcion, @imagen_url);
-        
-        -- Confirmar la transacción
-        COMMIT;
-        
-        PRINT 'Libro agregado exitosamente.';
-    END TRY
-    BEGIN CATCH
-        -- Si ocurre un error, hacer rollback de la transacción
-        ROLLBACK;
-        
-        PRINT 'Error al agregar el libro: ' + ERROR_MESSAGE();
-    END CATCH
-END
-GO
-
-
+    INSERT INTO Libros(titulo, autor, genero, año_publicacion, estado_L, descripcion, imagen)
+    VALUES (@titulo, @autor, @genero, @anio, @estado, @descripcion, @imagen);
+END;
 
 
