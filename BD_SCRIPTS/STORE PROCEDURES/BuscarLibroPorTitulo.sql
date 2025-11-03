e14febb8d94033b1d8234b3f9096c023b638d5ae
DCarhuamaca <140361129+Keterod@users.noreply.github.com>
2025-11-02 21:47:21 -0500

    Update BuscarLibroPorTitulo.sql

--- BD_SCRIPTS/STORE PROCEDURES/BuscarLibroPorTitulo.sql
+++ BD_SCRIPTS/STORE PROCEDURES/BuscarLibroPorTitulo.sql
@@ -2,34 +2,31 @@
 --  PROCEDIMIENTO ALMACENADO: BuscarLibroPorTitulo
 -- ===========================================================
 CREATE PROCEDURE BuscarLibroPorTitulo
-    @parte_titulo VARCHAR(150)  -- Parte del título para buscar
+    @parte_titulo VARCHAR(150)
 AS
 BEGIN
     SET NOCOUNT ON;
 
     BEGIN TRY
-        -- Buscar libros cuyo título contenga la palabra proporcionada
         SELECT 
             id_libro,
-            titulo, 
-            imagen_url, 
-            estado_L
+            titulo,
+            autor,
+            genero,
+            año_publicacion,
+            estado_L AS estado,
+            descripcion,
+            imagen -- columna tipo VARBINARY(MAX)
         FROM Libros
         WHERE titulo LIKE '%' + @parte_titulo + '%'
         ORDER BY titulo ASC;
-
-        -- Verificar si se encontraron resultados
-        IF @@ROWCOUNT = 0
-            PRINT 'No se encontraron libros con esa parte de título.';
-        ELSE
-            PRINT 'Búsqueda completada con éxito.';
     END TRY
-
     BEGIN CATCH
         PRINT 'Error al buscar libros: ' + ERROR_MESSAGE();
     END CATCH
 END;
 GO
+
 
 
 
